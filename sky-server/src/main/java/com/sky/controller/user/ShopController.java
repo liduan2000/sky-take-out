6a98535fee99fb2feae3a2ca406582b6b1546ea4
Nino163 <liduan2022@163.com>
2024-01-11 11:41:44 +0800

    USER - Shop - New API: get status

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.user;
+
+import com.sky.constant.ShopConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")
+public class ShopController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取营业状态
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(ShopConstant.SHOP_STATUS);
+        if (status != null) {
+            log.info("当前营业状态为：{}", status.equals(ShopConstant.OPEN) ? "营业中" : "打烊中");
+        }
+        return Result.success(status);
+    }
+}
