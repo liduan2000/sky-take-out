aa2e0c2d10bd00193cb6d2951e2b5ba7296bf424
Nino163 <liduan2022@163.com>
2024-01-11 11:39:38 +0800

    Common: Add ShopConstant class

---
+++ sky-common/src/main/java/com/sky/constant/ShopConstant.java
@@ -0,0 +1,14 @@
+package com.sky.constant;
+
+/**
+ * 店铺状态字段常量
+ */
+public class ShopConstant {
+    public static final String SHOP_STATUS = "SHOP_STATUS";
+
+    // 营业中
+    public static final Integer OPEN = 1;
+
+    // 打烊中
+    public static final Integer CLOSE = 0;
+}
