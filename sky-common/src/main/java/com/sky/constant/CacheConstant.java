dc888761dfd616ecef42eefd5ee1426b5ad5a031
Nino163 <liduan2022@163.com>
2024-01-15 15:12:36 +0800

    Common: add CacheConstant & enable spring-cache

---
+++ sky-common/src/main/java/com/sky/constant/CacheConstant.java
@@ -0,0 +1,6 @@
+package com.sky.constant;
+
+public class CacheConstant {
+    public static final String DISH_CACHE_NAME = "dish_";
+    public static final String SETMEAL_CACHE_NAME = "setmeal_";
+}
